902e801e8ce460fad33fee5f13fbce7f985befe0
Abby Garay <rgaray@serenova.com>
2019-05-07 14:25:41 -0600

    add version tag to response and add new container image to monolith pod

--- app/handlers/hello.go
+++ app/handlers/hello.go
@@ -7,10 +7,12 @@ import (
 
 type HelloResponse struct {
 	Message string `json:"message"`
+	Version string `json:version`
 }
 
 func HelloHandler(w http.ResponseWriter, r *http.Request) {
 	response := HelloResponse{
+		Version: "v1.0.1",
 		Message: "Hello... from my fork",
 	}
 	json.NewEncoder(w).Encode(response)
